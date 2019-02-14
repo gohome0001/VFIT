d6c14b592caea806ab55f8f66b569c8ac9cd3a6a
qqq1ppp <hakyongch@gmail.com>
2019-02-14 15:25:48 +0900

    have to fix & fixed code
    there are some error/bugs in this code. 
    SHOULD BE fixed.

--- proto.py
+++ proto.py
@@ -1,6 +1,6 @@
 import os, subprocess, sys
 
-TMPDIR = "./vfit/"
+TMPDIR = "./vfit"
 TMP_MERGE = "tmp_merge"
 TMP_MERGE_PATH = TMPDIR+'/'+TMP_MERGE
 TMP_SINGLE = "tmp_test.mp4"
@@ -11,13 +11,14 @@ OUTPUT_PATH = './output.mp4'
 def initialize():
     if not os.path.exists(TMPDIR):
         os.makedirs(TMPDIR)
-    f = open(TMP_MERGE_path)
+    os.system("touch "+TMP_MERGE_PATH)
+    f = open(TMP_MERGE_PATH)
     f.close()
     del f
 
-def get_metadata(file_path):
+def get_res(file_path):
     command = "ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1:nk=1 "+file_path
-    r = subprocess.check_output(command,shell=true)
+    r = subprocess.check_output(command,shell=True)
     return r.split('\n')
 # ffmpeg -i filename -f ffmetadata result.txt
 # filter with %dx%d (i.e. 1920x1080)
@@ -26,44 +27,61 @@ def get_metadata(file_path):
 
 def check_compatable(vid_path,img_path): # resolution check.
     img_res = get_res(img_path)
-    if get_res(vid_path) == image_res:
-        return image_res
+    if get_res(vid_path) == img_res:
+        return img_res
     else:
         return False
     
 
-def merge_vid(vid_path_list, save_path):
-    f = open(TMP_MERGE_PATH,'wb'):
-        for i in vid_path_list:
-            f.writeline("file '%s'" % i)
+def merge_vid(vid_path_list, save_path=OUTPUT_PATH):
+    f = open(TMP_MERGE_PATH,'wb')
+    for i in vid_path_list:
+        f.writelines("file '%s'\n" % ('.'+i[len('TMPDIR'):]))
     f.close()
-    command = "ffmpeg -f concat -safe 0 -i "+TMP_MERGE_PATH+" -c copy " + OUTPUT_PATH
-    r = subprocess.check_output(command, shell=truea)
+    command = "ffmpeg -f concat -safe 0 -i "+TMP_MERGE_PATH+" -c copy " + save_path
+    r = subprocess.check_output(command, shell=True) # for further use..?
+    return save_path
     # ffmpeg -f concat -safe 0 -i mylist.txt -c copy output
 
 def create_vid(image_path,fps,res):
-    command = "ffmpeg -r "+fps+" -f image2 -s "+res+" -i "+image_path+" -vframes 1 -vcodec libx264 -crf 25 -pix_fmt yuv420p "+TMP_SINGLE_PATH
-    subprocess.check_output(command, shell=true)
+    command = "ffmpeg -r "+str(fps)+" -f image2 -s "+res+" -i "+image_path+" -vframes 1 -vcodec libx264 -crf 25 -pix_fmt yuv420p "+TMP_SINGLE_PATH
+    subprocess.check_output(command, shell=True)
     return TMP_SINGLE_PATH
 # ffmpeg -r fps -f image2 -s revolution(i.e. 720x480) -i filename -vframes 1 -vcodec libx264 -crf 25 -pix_fmt yuv420p test.mp4
 
 
-def split_vid(vid_path, time1, time2):
-    commnad1 = "ffmpeg -i "+vid_path+".mp4 -acodec copy -vcodec copy -ss 00:00:00 -t "+time1+" "+TMP_MERGE_PATH+"1.mp4"
-    command2 = "ffmpeg -i "+vid_path+".mp4 -acodec copy -vcodec copy -ss "+time1+" -t "+time2+" "+TMP_MERGE_PATH+"2.mp4"
+def split_vid(vid_path, time1,time2=None):
+#    command1 = "ffmpeg -ss 00:00:00 -i "+vid_path+" -to "+time1+" -c copy -copyts "+TMP_MERGE_PATH+"1.mp4"
+#    command2 = "ffmpeg -ss "+time1+" -i "+vid_path+" -to "+time2+" -c copy -copyts "+TMP_MERGE_PATH+"2.mp4"
+
+    command1 = "ffmpeg -ss 00:00:00 -i "+vid_path+" -to 00:00:02 -c copy -copyts "+TMP_MERGE_PATH+"1.mp4"
+    command2 = "ffmpeg -ss 00:00:02 -i "+vid_path+" -to 00:00:02 -c copy "+TMP_MERGE_PATH+"2.mp4" 
+    # TODO : make second command work as it should be.
+
+    # os.system(command1)
+    os.system(command2)
     return [TMP_MERGE_PATH+"1.mp4", TMP_MERGE_PATH+"2.mp4"]
 # ffmpeg -i ORIGINALFILE.mp4 -acodec copy -vcodec copy -ss START -t LENGTH OUTFILE.mp4
 
-def main(vid_path, img_path):
-    img_res = check_compatabe(vid_path,img_path)
-    if !img_res:
+def main(vid_path, img_path, time1, time2):
+    initialize()
+    img_res = check_compatable(vid_path,img_path)
+    if not img_res:
         print "check the resolution of an image file"
         exit(1)
-    # 19.02.13 unfinished..
-
+    img_res = img_res[0]+"x"+img_res[1]
+    tvid2_path = create_vid(img_path,24,img_res)
+    tvid1_path, tvid3_path = split_vid(vid_path, time1, time2)
+    merge_path = [tvid1_path,tvid2_path,tvid3_path]
+    outvid_path = merge_vid(merge_path)
+    print "result : " + outvid_path
+    # 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print "usage : video_path image_path vid_split_time(hh:mm:ss.fff) vid_end_time"
     vid_path = sys.argv[1]
     img_path = sys.argv[2]
-    time = sys.argv[3]
-    main(vid_path, img_path, time)
+    time1 = sys.argv[3]
+    time2 = sys.argv[4]
+    main(vid_path, img_path, time1, time2)
